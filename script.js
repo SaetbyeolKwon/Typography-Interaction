c9f608723c0cefe54c4ac052c6dc9f05c8422df3
saetbyeolkwon <kwons201@newschool.edu>
2018-11-14 01:13:04 -0500

    Nov14

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const texts = ["<a href = 'http://octhomework.glitch.me/' target = '_blank'>jQuery Practice</a>"
+const texts = ["<a href = 'https://solar-substance.glitch.me/' target = '_blank'>Box Model Example</a>"
+,"<a href = 'http://octhomework.glitch.me/' target = '_blank'>jQuery Exercise</a>"
 ,"<a href = 'https://saetbyeolkwon.github.io/responsiveWeb/' target = '_blank'>Responsive Web1</a>"
 ,"<a href = 'https://saetbyeolkwon.github.io/Bomb-web/' target = '_blank'>Responsive Web2</a>"
-,"<a href = 'http://octhomework.glitch.me/' target = '_blank'>jQuery Practice</a><br><a href = 'https://saetbyeolkwon.github.io/responsiveWeb/' target = '_blank'>Responsive Web1</a><br><a href = 'https://saetbyeolkwon.github.io/Bomb-web/' target = '_blank'>Responsive Web2</a>"
+,"<a href = 'https://solar-substance.glitch.me/' target = '_blank'>Box Model Example</a><br><a href = 'http://octhomework.glitch.me/' target = '_blank'>jQuery Practice</a><br><a href = 'https://saetbyeolkwon.github.io/responsiveWeb/' target = '_blank'>Responsive Web1</a><br><a href = 'https://saetbyeolkwon.github.io/Bomb-web/' target = '_blank'>Responsive Web2</a>"
 ]
 
 let index = 0;
@@ -15,7 +16,7 @@ let index = 0;
     $("h2").html(texts[index]).css("font-size","80px").css("line-height","120px")
     index++
 
-    if(index > 3){
+    if(index > 4){
         index = 0;
     }
 })
