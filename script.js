7e4975f1a1aff31d8c9e630fd9ae89259ecd9e42
Saetbyeol Kwon <43006719+SaetbyeolKwon@users.noreply.github.com>
2018-11-14 01:03:01 -0500

    Add files via upload
    Nov14

---
+++ script.js
@@ -0,0 +1,21 @@
+const texts = ["<a href = 'http://octhomework.glitch.me/' target = '_blank'>jQuery Practice</a>"
+,"<a href = 'https://saetbyeolkwon.github.io/responsiveWeb/' target = '_blank'>Responsive Web1</a>"
+,"<a href = 'https://saetbyeolkwon.github.io/Bomb-web/' target = '_blank'>Responsive Web2</a>"
+,"<a href = 'http://octhomework.glitch.me/' target = '_blank'>jQuery Practice</a><br><a href = 'https://saetbyeolkwon.github.io/responsiveWeb/' target = '_blank'>Responsive Web1</a><br><a href = 'https://saetbyeolkwon.github.io/Bomb-web/' target = '_blank'>Responsive Web2</a>"
+]
+
+let index = 0;
+
+  $(Document).click(function(){
+    console.log(index)
+    $("h1").hide()
+    $("h2").hide()
+    // $("h2").fadeOut("slow")
+    $("h2").fadeIn(1000)
+    $("h2").html(texts[index]).css("font-size","80px").css("line-height","120px")
+    index++
+
+    if(index > 3){
+        index = 0;
+    }
+})
